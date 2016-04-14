3f2075f45515a9228999bef126575fd1603eb008
lwtllcm <laurie@Lauries-Air.attlocal.net>
2016-04-13 23:28:14 -0500

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Laurie Wheeler on 4/13/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
